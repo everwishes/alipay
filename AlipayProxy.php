968722ac908c9bc3420270c68d109dbd044b10fa
xc <xc@11.com>
2015-04-23 13:20:19 +0800

    new

---
+++ AlipayProxy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace alipay;
+
+use yii\base\Component;
+
+class AlipayProxy extends Component
+{
+    private $_input_charset = "utf-8";
+    private $sign_type = "MD5";
+    private $transport = "http";
+
+    public $key = "";
+    public $partner = "";
+    public $seller_email = "";
+    public $return_url = "";
+    public $notify_url = "";
+    public $show_url = "";
+
+    public function init()
+    {
+        require_once(__DIR__ . '/class/*');
+    }
+
+    public function buildForm($request)
+    {
+        $params = array(
+            'partner'=>$this->partner,
+            'seller_email'=>$this->seller_email,
+            'return_url'=>$this->return_url,
+            'notify_url'=>$this->notify_url,
+            '_input_charset'=>$this->_input_charset,
+            'show_url'=>$this->show_url,
+        );
+        $params = array_merge($params, $request->getParams());
+        $service = new AlipayService($params, $this->key, $this->sign_type);
+        //测试逻辑
+        echo "<script>location.href='index.php?r=pay%2Falipay-return';</script>"; 
+        return $service->build_form();
+    }
+
+    public function verifyNotify()
+    {
+        $notify = new AlipayNotify($this->partner, $this->key, $this->sign_type, $this->_input_charset, $this->transport);
+        //测试逻辑
+        return true;
+        return $notify->notify_verify();
+    }
+
+    public function verifyReturn()
+    {
+        $notify = new AlipayNotify($this->partner, $this->key, $this->sign_type, $this->_input_charset, $this->transport);
+        //测试逻辑
+        return true;
+        return $notify->return_verify();
+    }
+}
+
